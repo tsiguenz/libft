81d08049ad685bb2b5f4643e1a83dc7a19cf61e2
Siguenza92 <thibaut.siguenza@gmail.com>
2021-11-02 18:57:59 +0100

    push

--- libft.h
+++ libft.h
@@ -6,27 +6,27 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 23:22:38 by thibautsi         #+#    #+#             */
-/*   Updated: 2021/10/13 15:15:57 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2021/10/27 16:45:24 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_strlen(const char *s);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(const char *s);
 //ft_memset();
 //ft_bzero();
 //ft_memcpy();
 //ft_memmove();
 //ft_strlcpy();
 //ft_strlcat();
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 
 #endif //LIBFT_H
