f302eb89b0f6477db3c27f958d066f5b2ab37839
Siguenza92 <thibaut.siguenza@gmail.com>
2021-11-17 23:16:11 +0100

    repush

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 15:11:38 by tsiguenz          #+#    #+#             */
+/*   Updated: 2021/11/17 15:52:48 by tsiguenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned int	i;
+	char	*p_s;
+	
+	i = 0;
+	p_s = (char *) s;
+	if (*p_s == c)
+		return (p_s);
+	while (i < n)
+	{
+		if (p_s[i] == c)
+			return (&p_s[i]);
+		i++;
+	}
+	return (0);
+}
---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 16:08:13 by tsiguenz          #+#    #+#             */
+/*   Updated: 2021/11/17 16:13:12 by tsiguenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned int	i;
+	char		*p_s1;
+	char		*p_s2;
+
+	i = 0;
+	p_s1 = (char *) s1;
+	p_s2 = (char *) s2;
+	while (i < n)
+	{
+		if (p_s1[i] != p_s2[i])
+			return (p_s1[i] - p_s2[i]);
+		i++;
+	}
+	return (0);
+}
